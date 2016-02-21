85c9789c344ec7b01aa07b9c7a6727274c91c7c6
Iker Arbulu Lozano <ikerarbulu@gmail.com>
2016-02-20 18:47:33 -0600

    Added dropdowns with materialize

--- client/certex.js
+++ client/certex.js
@@ -2,18 +2,22 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+  Template.createCertificate.helpers({
+    publicKeyHost: function(){
+      return "Girresawgrntej6uj"
+    },
+    certificateCategories:[
+      {category: "category 1"},
+      {category: "category 2"},
+      {category: "category 3"}
+    ],
+    certificateTemplate:[
+      {title: "title 1"},
+      {title: "title 2"},
+      {title: "title 3"}
+    ]
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 }
 
 if (Meteor.isServer) {
